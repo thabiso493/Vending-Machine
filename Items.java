06ce135e32032e5c2bf2b8b6a6ba0b5c879c71d7
thabiso493 <57901905+thabiso493@users.noreply.github.com>
2020-03-05 04:54:32 -0800

    Add files via upload

---
+++ Items.java
@@ -0,0 +1,33 @@
+package VandingMachine;
+
+public class Items {
+	private String name;
+
+	private double price;
+
+	private double quantity;
+
+	public Items(String name, double price, double quantity) {
+		this.name = name;
+
+		this.price = price;
+
+		this.quantity = quantity;
+	}
+
+	public void setQuantity(double newQuantity) {
+		quantity = newQuantity;
+	}
+
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
